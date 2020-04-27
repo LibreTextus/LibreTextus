294d7e5b5616c1267c45e7894acebfed1ccc4819
Nathan Reiner <nathan.p.reiner@gmail.com>
2020-04-27 15:28:28 +0200

    add snapcraft test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <gtkmm.h>
 #include <X11/Xlib.h>
 #include <iostream>
+#include <experimental/filesystem>
 
 #include "framework.hpp"
 
 int main(int argc, char *argv[]) {
+
+	if (getenv("SNAP")) {
+		std::experimental::filesystem::current_path(getenv("SNAP"));
+	}
 
 	XInitThreads();
 

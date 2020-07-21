62dd89363b98998d28c947903d89c0d652c07972
NPScript <nathan.p.reiner@gmail.com>
2020-07-21 21:01:38 +0200

    Use linkname /proc/self/exe to get exec path

--- src/path.cpp
+++ src/path.cpp
@@ -1,14 +1,25 @@
 #include "path.hpp"
-#include <iostream>
 
 std::string DATA(const std::string & object) {
 	std::string output;
+
+#ifdef __linux__
+	std::vector<char> buf(400);
+	ssize_t len;
+	
+	do {
+		buf.resize(buf.size() + 100);
+		len = readlink("/proc/self/exe", &(buf[0]), buf.size());
+	} while (buf.size() == len);
+
+	buf[len] = '\0';
+	output = std::string(&(buf[0]));
+
+	output = output.substr(0, output.find_last_of("/") + 1);
+	output += "data/" + object;
+#else	
 	output = "data/" + object;
-	#ifndef __LINUX__
-	if (getenv("SNAP")) {
-		output = getenv("SNAP") + ("/" + output);
-	}
-	#endif
+#endif
 
 	return output;
 }
--- src/path.hpp
+++ src/path.hpp
@@ -2,6 +2,9 @@
 #define __LIBRE_PATH__
 
 #include <string>
+#include <unistd.h>
+#include <vector>
+#include <string>
 
 std::string DATA(const std::string & object);
 std::string HOME(const std::string & object);

505f317b8b74bbab35a0c188d04977e01153011a
NPScript <nathan.p.reiner@gmail.com>
2020-12-10 16:49:37 +0100

    about window add logo

--- src/window/about/about.cpp
+++ src/window/about/about.cpp
@@ -2,12 +2,15 @@
 #include "gtkmm/enums.h"
 
 Libre::AboutWindow::AboutWindow()
-	: info("Version: " + std::string(VERSION) + ", " + std::string(GIT_COMMIT_ID)) {
-		this->box.pack_start(this->info);
+	:	logo(Gdk::Pixbuf::create_from_file(DATA("icon.svg"), 100, 100, true)),
+		info("Version: " + std::string(VERSION) + ", " + std::string(GIT_COMMIT_ID)) {
+
+		this->box.pack_start(this->logo, Gtk::PACK_SHRINK, 5);
+		this->box.pack_start(this->info, Gtk::PACK_SHRINK, 5);
 		this->add(this->box);
 
 		this->set_title("About");
-		this->set_default_size(300, 100);
+		this->set_border_width(20);
 		this->set_keep_above(true);
 		this->set_resizable(false);
 		this->set_position(Gtk::WIN_POS_CENTER);
--- src/window/about/about.hpp
+++ src/window/about/about.hpp
@@ -2,12 +2,14 @@
 #define LIBRE_ABOUT_WINDOW_HPP
 
 #include <gtkmm.h>
+#include <path/path.hpp>
 
 namespace Libre {
 	class AboutWindow : public Gtk::Window {
 		private:
-			Gtk::HBox box;
+			Gtk::VBox box;
 			Gtk::Label info;
+			Gtk::Image logo;
 
 		public:
 			AboutWindow();
